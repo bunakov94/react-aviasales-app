baa683087cfc57b90249154b30c96e549c47b245
bunakov94 <bunakov.code@gmail.com>
2021-01-30 18:08:40 +0300

    3.2.6 fix radio buttons in Tabs

--- src/components/blocks/Tabs/Tabs.tsx
+++ src/components/blocks/Tabs/Tabs.tsx
@@ -3,12 +3,12 @@ import classes from './Tabs.module.scss';
 
 const Tabs = () => (
   <div className={classes.tabs}>
-    <input type="radio" className={classes.input} name="radio" aria-label="radio-button" />
-    <label htmlFor="radio" className={`${classes.label} ${classes.label__active}`}>
+    <input type="radio" className={classes.input} name="radio" aria-label="radio-button" defaultChecked />
+    <label htmlFor="radio" className={classes.label}>
       Самый дешевый
     </label>
-    <input type="radio" className={classes.input} name="radio2" aria-label="radio-button" />
-    <label htmlFor="radio2" className={classes.label}>
+    <input type="radio" className={classes.input} name="radio" aria-label="radio-button" />
+    <label htmlFor="radio" className={classes.label}>
       Самый быстрый
     </label>
   </div>
