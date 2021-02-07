54d4874a5792e33a94e4629904e9fa2765d4ab7f
bunakov94 <bunakov.code@gmail.com>
2021-02-08 01:06:49 +0300

    refactor filters+

--- src/actions/actions.ts
+++
@@ -1,5 +0,0 @@
-export const all = (payload: any) => ({ type: 'ALL', payload });
-export const nothing = (payload: any) => ({ type: 'NOTHING', payload });
-export const one = (payload: any) => ({ type: 'ONE', payload });
-export const two = (payload: any) => ({ type: 'TWO', payload });
-export const three = (payload: any) => ({ type: 'THREE', payload });
---
+++ src/actions/filters.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const checkFunc = (type: string, all: boolean = false) => ({ type, payload: all });
