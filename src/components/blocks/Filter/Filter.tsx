2fa953708b43ea312fac2c026c365a1c6177ccb1
bunakov94 <bunakov.code@gmail.com>
2021-02-08 19:24:13 +0300

    refactoring the filters component

--- src/components/blocks/Filter/Filter.tsx
+++ src/components/blocks/Filter/Filter.tsx
@@ -1,40 +1,35 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../../actions/filters';
 import classes from './Filter.module.scss';
 
-const Filter = ({ filters, checkFunc }: any) => {
-  useEffect(() => {
-    let count = 0;
-    for (const filter in filters) {
-      if (Object.prototype.hasOwnProperty.call(filters, filter) && filters[filter].checked === true) {
-        count += 1;
-      }
-    }
-    if (count === 4) {
-      checkFunc('ALL');
-    }
-  }, [filters, checkFunc]);
-
-  return (
-    <div className={classes.filter}>
-      <h1 className={classes.filter__title}>Количество пересадок</h1>
-
-      {Object.keys(filters).map((filter: any) => {
-        const { type, title, checked } = filters[filter];
-        return (
-          <label className={classes.item} key={title}>
-            <input className={classes.itemInput} type="checkbox" checked={checked} onChange={() => checkFunc(type)} />
-            <span className={classes.itemCheckmark} />
-            <span className={classes.itemLabel}>{title}</span>
-          </label>
-        );
-      })}
-    </div>
-  );
+type Filters = {
+  [key: string]: { type: string; title: string; checked: boolean };
 };
 
-const mapStateToProps = (state: any) => ({
+interface IFilterProps {
+  filters: Filters;
+  checkFunc: (type: string) => { type: string };
+}
+
+const Filter = ({ filters, checkFunc }: IFilterProps) => (
+  <div className={classes.filter}>
+    <h1 className={classes.filter__title}>Количество пересадок</h1>
+
+    {Object.keys(filters).map((filter: any) => {
+      const { type, title, checked } = filters[filter];
+      return (
+        <label className={classes.item} key={title}>
+          <input className={classes.itemInput} type="checkbox" checked={checked} onChange={() => checkFunc(type)} />
+          <span className={classes.itemCheckmark} />
+          <span className={classes.itemLabel}>{title}</span>
+        </label>
+      );
+    })}
+  </div>
+);
+
+const mapStateToProps = (state: { filters: Filters }) => ({
   filters: state.filters,
 });
 
--- src/reducers/filters.ts
+++ src/reducers/filters.ts
@@ -1,48 +1,55 @@
+const checkFunk = (state: Filters, type: string) => {
+  const filterName = type.toLowerCase();
+  const result = {
+    ...state,
+    [filterName]: { ...state[filterName], checked: !state[filterName].checked },
+    all: { ...state.all, checked: false },
+  };
+
+  let count = 0;
+  for (const filter in result) {
+    if (Object.prototype.hasOwnProperty.call(result, filter) && result[filter].checked === true) {
+      count += 1;
+    }
+  }
+
+  if (filterName === 'all' || count === 4) {
+    return Object.keys(state).reduce((acc: Filters, el) => {
+      acc[el] = { ...state[el], checked: !state.all.checked };
+      return acc;
+    }, {});
+  }
+
+  return result;
+};
+
 export default function filters(
-  state: any = {
+  state: Filters = {
     all: { type: 'ALL', title: 'Все', checked: false },
     nothing: { type: 'NOTHING', title: 'Без пересадок', checked: false },
     one: { type: 'ONE', title: '1 пересадка', checked: false },
     two: { type: 'TWO', title: '2 пересадка', checked: false },
     three: { type: 'THREE', title: '3 пересадка', checked: false },
   },
-  action: any,
+  action: { type: string },
 ) {
   switch (action.type) {
     case 'ALL':
-      return {
-        all: { ...state.all, checked: !state.all.checked },
-        nothing: { ...state.nothing, checked: !state.all.checked },
-        one: { ...state.one, checked: !state.all.checked },
-        two: { ...state.two, checked: !state.all.checked },
-        three: { ...state.three, checked: !state.all.checked },
-      };
+      return checkFunk(state, action.type);
     case 'NOTHING':
-      return {
-        ...state,
-        nothing: { ...state.nothing, checked: !state.nothing.checked },
-        all: { ...state.all, checked: action.payload },
-      };
+      return checkFunk(state, action.type);
     case 'ONE':
-      return {
-        ...state,
-        one: { ...state.one, checked: !state.one.checked },
-        all: { ...state.all, checked: action.payload },
-      };
+      return checkFunk(state, action.type);
     case 'TWO':
-      return {
-        ...state,
-        two: { ...state.two, checked: !state.two.checked },
-        all: { ...state.all, checked: action.payload },
-      };
+      return checkFunk(state, action.type);
     case 'THREE':
-      return {
-        ...state,
-        three: { ...state.three, checked: !state.three.checked },
-        all: { ...state.all, checked: action.payload },
-      };
+      return checkFunk(state, action.type);
 
     default:
       return state;
   }
 }
+
+type Filters = {
+  [key: string]: { type: string; title: string; checked: boolean };
+};
